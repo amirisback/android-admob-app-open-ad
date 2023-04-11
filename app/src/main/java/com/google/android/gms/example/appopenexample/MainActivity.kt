52eaedcc2cb0fe0f523bb1fa1da4f9fd2b163cae
Muhammad Faisal Amir <faisalamircs@gmail.com>
2023-04-11 14:10:51 +0700

    update: tidy up code

--- app/src/main/java/com/google/android/gms/example/appopenexample/MainActivity.kt
+++ app/src/main/java/com/google/android/gms/example/appopenexample/MainActivity.kt
@@ -6,15 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 /** The main activity in the app. */
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 
-  /** Override the default implementation when the user presses the back key. */
-  override fun onBackPressed() {
-    // Move the task containing the MainActivity to the back of the activity stack, instead of
-    // destroying it. Therefore, MainActivity will be shown when the user switches back to the app.
-    moveTaskToBack(true)
-  }
+    /** Override the default implementation when the user presses the back key. */
+    override fun onBackPressed() {
+        // Move the task containing the MainActivity to the back of the activity stack, instead of
+        // destroying it. Therefore, MainActivity will be shown when the user switches back to the app.
+        moveTaskToBack(true)
+    }
 }
--- app/src/main/java/com/google/android/gms/example/appopenexample/SplashActivity.kt
+++ app/src/main/java/com/google/android/gms/example/appopenexample/SplashActivity.kt
@@ -18,59 +18,59 @@ private const val LOG_TAG = "SplashActivity"
 /** Splash Activity that inflates splash activity xml. */
 class SplashActivity : AppCompatActivity() {
 
-  private var secondsRemaining: Long = 0L
+    private var secondsRemaining: Long = 0L
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_splash)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
-    // Create a timer so the SplashActivity will be displayed for a fixed amount of time.
-    createTimer(COUNTER_TIME)
-  }
-
-  /**
-   * Create the countdown timer, which counts down to zero and show the app open ad.
-   *
-   * @param seconds the number of seconds that the timer counts down from
-   */
-  private fun createTimer(seconds: Long) {
-    val counterTextView: TextView = findViewById(R.id.timer)
-    val countDownTimer: CountDownTimer = object : CountDownTimer(seconds * 1000, 1000) {
-      override fun onTick(millisUntilFinished: Long) {
-        secondsRemaining = millisUntilFinished / 1000 + 1
-        counterTextView.text = "App is done loading in: $secondsRemaining"
-      }
-
-      override fun onFinish() {
-        secondsRemaining = 0
-        counterTextView.text = "Done."
-
-        val application = application as? MyApplication
-
-        // If the application is not an instance of MyApplication, log an error message and
-        // start the MainActivity without showing the app open ad.
-        if (application == null) {
-          Log.e(LOG_TAG, "Failed to cast application to MyApplication.")
-          startMainActivity()
-          return
-        }
-
-        // Show the app open ad.
-        application.showAdIfAvailable(
-          this@SplashActivity,
-          object : MyApplication.OnShowAdCompleteListener {
-            override fun onShowAdComplete() {
-              startMainActivity()
-            }
-          })
-      }
+        // Create a timer so the SplashActivity will be displayed for a fixed amount of time.
+        createTimer(COUNTER_TIME)
     }
-    countDownTimer.start()
-  }
 
-  /** Start the MainActivity. */
-  fun startMainActivity() {
-    val intent = Intent(this, MainActivity::class.java)
-    startActivity(intent)
-  }
+    /**
+     * Create the countdown timer, which counts down to zero and show the app open ad.
+     *
+     * @param seconds the number of seconds that the timer counts down from
+     */
+    private fun createTimer(seconds: Long) {
+        val counterTextView: TextView = findViewById(R.id.timer)
+        val countDownTimer: CountDownTimer = object : CountDownTimer(seconds * 1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                secondsRemaining = millisUntilFinished / 1000 + 1
+                counterTextView.text = "App is done loading in: $secondsRemaining"
+            }
+
+            override fun onFinish() {
+                secondsRemaining = 0
+                counterTextView.text = "Done."
+
+                val application = application as? MyApplication
+
+                // If the application is not an instance of MyApplication, log an error message and
+                // start the MainActivity without showing the app open ad.
+                if (application == null) {
+                    Log.e(LOG_TAG, "Failed to cast application to MyApplication.")
+                    startMainActivity()
+                    return
+                }
+
+                // Show the app open ad.
+                application.showAdIfAvailable(
+                    this@SplashActivity,
+                    object : MyApplication.OnShowAdCompleteListener {
+                        override fun onShowAdComplete() {
+                            startMainActivity()
+                        }
+                    })
+            }
+        }
+        countDownTimer.start()
+    }
+
+    /** Start the MainActivity. */
+    fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
 }
